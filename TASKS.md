d0305d445e84a518f4117e9bbe23b3d0fa4498bf
Kunal <kunalkhan370@gmail.com>
2024-08-23 23:41:34 +0530

    Fixed pagination url query

--- TASKS.md
+++ TASKS.md
@@ -1,9 +1,15 @@
 # Pending Tasks
-- Fix pagination & other urls of the form data page
 - Add Export csv option to the form data
 - Forgot password option
+- Change the validation rules input of fields
+- Add File upload option *
+- Commenting on Controller Functions
+- Commenting on Blade Templates
+- Create a lots of data & test tables & set configurations
+- Redesin the UI *
 
 # Completed Tasks
+- Fix pagination & other urls of the form data page
 - Make the UI responsive
 - Add filter to the form data
 - Add search to the form data
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -84,16 +84,16 @@
         <nav aria-label="Page navigation example">
             <ul class="inline-flex -space-x-px text-sm">
               <li>
-                <a href="{{$users->previousPageUrl()}}" class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                <a href="{{$users->appends(request()->except('page'))->previousPageUrl()}}" class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
               </li>
               @for($i = 1; $i <= $users->lastPage(); $i++)
               <li>
-                <a href="{{$users->url($i)}}" class="{{$i == $users->currentPage() ? 'flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white' : 'flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'}}">{{$i}}</a>
+                <a href="{{$users->appends(request()->except('page'))->url($i)}}" class="{{$i == $users->currentPage() ? 'flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white' : 'flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'}}">{{$i}}</a>
               </li>
               @endfor
               
               <li>
-                <a href="{{$users->nextPageUrl()}}" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                <a href="{{$users->appends(request()->except('page'))->nextPageUrl()}}" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
               </li>
             </ul>
           </nav>
