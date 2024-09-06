c4ec1d6af67996219631cd4bf1d8f06b21252e65
Kunal <kunalkhan370@gmail.com>
2024-09-07 00:20:38 +0530

    Upgrade Validation rules controls for fields

--- resources/views/fields/form.blade.php
+++ resources/views/fields/form.blade.php
@@ -80,75 +80,74 @@
                 <div class="flex flex-col gap-4">
                     <p class="block text-sm font-medium text-gray-900 dark:text-white">Validation rule</p>
 
-                    <div class="flex items-center" v-show="!email && !file && !boolean">
-                        <input v-model="numeric" id="vr-numeric" name="validation_rules[]"
+                    <div class="flex items-center">
+                        <input :disabled="email || file || boolean" v-model="numeric" id="vr-numeric" name="validation_rules[]"
                             type="checkbox" value="numeric"
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            class="v-checkbox">
                         <label for="vr-numeric"
-                            class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Number</label>
-                    </div>
-
-                    <div class="flex items-center" v-show="!numeric && !file && !boolean">
-                        <input v-model="email" id="vr-email" name="validation_rules[]"
-                            type="checkbox" value="email"
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="vr-email"
-                            class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
-                    </div>
-
-                    <div class="flex items-center" v-show="!email && !numeric && !boolean">
-                        <input v-model="file" id="vr-file" name="validation_rules[]"
-                            type="checkbox" value="file"
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="vr-file" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">File</label>
-                    </div>
-
-                    <div class="flex items-center" v-show="!email && !numeric && !file">
-                        <input disabled v-model="boolean" id="vr-checkbox" name="validation_rules[]"
-                            type="checkbox" value="boolean"
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="vr-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checkbox
-                            (yes/no)</label>
-                    </div>
-
-                    <div class="flex items-center" v-show="!boolean">
-                        <input v-model="max" id="vr-max" name="validation_rules[]" type="checkbox" value="max"
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="vr-max" class="mx-2 text-sm font-medium text-gray-900 dark:text-gray-300">Max
-                            @{{file ? 'Size (KB)' : 'Length'}}</label>
-                        <input type="number" name="max" value="{{ $rules['max'] }}"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-14 p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-50">
-                    </div>
-
-                    <div class="flex items-center" v-show="!boolean">
-                        <input v-model="min" id="vr-min" name="validation_rules[]"
-                            type="checkbox" value="min"
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="vr-min" class="mx-2 text-sm font-medium text-gray-900 dark:text-gray-300">Min
-                            @{{file ? 'Size (KB)' : 'Length'}}</label>
-                        <input type="number" name="min" value="{{ $rules['min'] }}"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-14 p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-50">
-                    </div>
-
-                    <div class="flex items-center" v-show="numeric">
-                        <input v-model="digits" id="vr-digits" name="validation_rules[]" 
-                            type="checkbox" value="digits"
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="vr-digits"
-                            class="mx-2 text-sm font-medium text-gray-900 dark:text-gray-300">Digits</label>
-                        <input type="number" name="digits" value="{{ $rules['digits'] }}"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-14 p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-50">
+                            class="v-label" :class="{disabled: email || file || boolean}">Number</label>
                     </div>
 
                     <div class="flex items-center">
-                        <input v-model="inArr" id="vr-in" name="validation_rules[]"
+                        <input :disabled="numeric || file || boolean" v-model="email" id="vr-email" name="validation_rules[]"
+                            type="checkbox" value="email"
+                            class="v-checkbox">
+                        <label for="vr-email"
+                            class="v-label" :class="{disabled: numeric || file || boolean}">Email</label>
+                    </div>
+
+                    <div class="flex items-center">
+                        <input :disabled="email || numeric || boolean" v-model="file" id="vr-file" name="validation_rules[]"
+                            type="checkbox" value="file"
+                            class="v-checkbox">
+                        <label for="vr-file" class="v-label" :class="{disabled: email || numeric || boolean}">File</label>
+                    </div>
+
+                    <div class="flex items-center">
+                        <input :disabled="email || numeric || file" v-model="boolean" id="vr-checkbox" name="validation_rules[]"
+                            type="checkbox" value="boolean"
+                            class="v-checkbox">
+                        <label for="vr-checkbox" class="v-label" :class="{disabled: email || numeric || file}">Checkbox (yes/no)</label>
+                    </div>
+
+                    <div class="flex items-center">
+                        <input :disabled="boolean" v-model="max" id="vr-max" name="validation_rules[]" type="checkbox" value="max"
+                            class="v-checkbox">
+                        <label for="vr-max" class="v-label" :class="{disabled: boolean}">Max
+                            @{{file ? 'Size (KB)' : 'Length'}}</label>
+                        <input type="number" name="max" value="{{ $rules['max'] }}" :disabled="boolean"
+                            class="v-input max-w-14">
+                    </div>
+
+                    <div class="flex items-center">
+                        <input :disabled="boolean" v-model="min" id="vr-min" name="validation_rules[]"
+                            type="checkbox" value="min"
+                            class="v-checkbox">
+                        <label for="vr-min" class="v-label" :class="{disabled: boolean}">Min
+                            @{{file ? 'Size (KB)' : 'Length'}}</label>
+                        <input type="number" name="min" value="{{ $rules['min'] }}" :disabled="boolean"
+                            class="v-input max-w-14">
+                    </div>
+
+                    <div class="flex items-center">
+                        <input :disabled="!numeric" v-model="digits" id="vr-digits" name="validation_rules[]" 
+                            type="checkbox" value="digits"
+                            class="v-checkbox">
+                        <label for="vr-digits" :class="{disabled: !numeric}"
+                            class="v-label">Digits</label>
+                        <input type="number" name="digits" value="{{ $rules['digits'] }}" :disabled="!numeric"
+                            class="v-input max-w-14">
+                    </div>
+
+                    <div class="flex items-center">
+                        <input :disabled="boolean" v-model="inArr" id="vr-in" name="validation_rules[]"
                             type="checkbox" value="in"
-                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="vr-in"
-                            class="mx-2 text-sm font-medium text-gray-900 dark:text-gray-300">List</label>
-                        <input type="text" name="in" value="{{ $rules['in'] }}"
+                            class="v-checkbox">
+                        <label for="vr-in" :class="{disabled: boolean}"
+                            class="v-label">List</label>
+                        <input type="text" name="in" value="{{ $rules['in'] }}" :disabled="boolean"
                             placeholder="Comma Separated Value(s)"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full max-w-64 p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-50">
+                            class="v-input max-w-64">
                     </div>
 
 
